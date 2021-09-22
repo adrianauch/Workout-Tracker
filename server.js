bf729b18a8af389fcac93ed8ef04c93ef5bf24b7
Adrian <adrian.auch11@gmail.com>
2021-09-22 15:33:50 -0600

    deploying

--- server.js
+++ server.js
@@ -16,8 +16,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 // Mongoose:
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+//   useNewUrlParser: true,
+// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 app.use(require("./routes/api-routes"));
